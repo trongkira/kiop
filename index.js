6c2fe8685d6794008e65de8fea50191de2abeca1
Kira <kira.trong@feavn.com.vn>
2018-11-27 15:44:04 +0700

    update rkbutton
    update rkbutton

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
 export { RkButton } from './src/components/button/rkButton';
+export { RkText } from './src/components/text/rkText';
--- src/components/button/rkButton.js
+++ src/components/button/rkButton.js
@@ -8,3 +8,66 @@ import _ from 'lodash';
 
 import { RkText } from '../text/rkText';
 import { RkComponent } from '../rkComponent';
+
+export class RkButton extends RkComponent {
+  static propTypes = {
+    style: ViewPropTypes.style,
+    contentStyle: ViewPropTypes.style,
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node,
+    ]),
+    touchable: PropTypes.func,
+  };
+  static defaultProps = {
+    style: null,
+    contentStyle: null,
+    children: [],
+    touchable: TouchableOpacity,
+  };
+  static contextTypes = {
+    theme: PropTypes.object,
+  };
+  componentName = 'RkButton';
+  typeMapping = {
+    container: {
+      hitSlop: 'hitSlop',
+    },
+    content: {
+      color: 'color',
+      fontSize: 'fontSize',
+    },
+  };
+
+  renderChildren(style) {
+    const displayText = (text) => (
+      <RkText style={[style, this.props.contentStyle]}>{text}</RkText>);
+    if (typeof this.props.children === 'string') {
+      return displayText(this.props.children);
+    }
+    const babies = _.isArray(this.props.children) ? this.props.children : [this.props.children];
+    return React.Children.map(babies, (baby) => {
+      if (typeof baby === 'string') {
+        return displayText(baby);
+      }
+      const { style: babyStyle, ...babyProps } = baby.props;
+      return React.cloneElement(baby, {
+        style: [this.props.contentStyle, babyStyle],
+        ...babyProps,
+      });
+    });
+  }
+
+  render() {
+    const { container, content } = super.defineStyles();
+    const { style, touchable: Touchable, ...touchableProps } = this.props;
+    const hitSlop = this.extractNonStyleValue(container, 'hitSlop');
+    if (hitSlop) touchableProps.hitSlop = hitSlop;
+
+    return (
+      <Touchable style={[container, style]} {...touchableProps}>
+        {this.props.children && this.renderChildren(content)}
+      </Touchable>
+    );
+  }
+}
